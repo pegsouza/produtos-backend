95e3faf952bafa50efa69ba76880938a879e8eab
pegsouza <paulo_14ed@hotmail.com>
2020-02-19 01:21:15 +0000

    aula 03

--- index.js
+++ index.js
@@ -7,18 +7,46 @@ var produtos = [
     {id: 2, nome: 'Mouse', preco: 20.50},
     {id: 4, nome: 'Teclado', preco: 220.50},
 ];
+
+// middlewares:
+server.use(express.json());
     
-server.get('/produtos', function(request, response) {
+server.get('/produto', function(request, response) {
     return response.json(produtos);
 })
 
-server.get('/produtos/:id', function(request, response) {
+server.get('/produto/:id', function(request, response) {
 
     const id = request.params.id;
 
-    const produto = produtos.filter(p => p.id==id);
-    return response.json(produtos);
+    const produto = produtos.filter(p => p.id == id);
+    return response.json(produto);
 })
 
+server.post('/produto', function(request, response){
+    const produto = request.body;
+    produtos.push(produto);
+    return response.status(201).send();
+})
+
+server.delete('/produto/:id', function(request, response){
+    const id = request.params.id;
+    produtos = produtos.filter(p => p.id != id);
+    return response.status(200).send();
+})
+
+server.put('/produto/:id', (request, response) =>{
+    const id = request.params.id;
+    const produto = request.body;
+
+    produtos.forEach(p =>{
+        if(p.id == id){
+            p.nome = produto.nome;
+            p.preco = produto.preco;
+            return;
+        }
+    })
+    return response.send();
+})
 
 server.listen(3000);
